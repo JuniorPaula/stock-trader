0604cf68cc174e83e9c5d4f6d784884f7d8c759c
Junior Paula <luke.junnior@icloud.com>
2023-10-23 09:17:21 -0300

    [API] Add stock model

---
+++ api/internal/models/stock.go
@@ -0,0 +1,12 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Stock struct {
+	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name  string             `json:"name,omitempty" bson:"name,omitempty"`
+	Price float64            `json:"price,omitempty" bson:"price,omitempty"`
+
+	CreatedAt string `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt string `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+}
