4bdf529b8a535a8804eabf176c38e5a78eb109c6
Junior Paula <luke.junnior@icloud.com>
2023-10-23 14:24:23 -0300

    [API] create repository method to list all stocks

--- api/internal/models/stock.go
+++ api/internal/models/stock.go
@@ -6,7 +6,4 @@ type Stock struct {
 	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name  string             `json:"name,omitempty" bson:"name,omitempty"`
 	Price float64            `json:"price,omitempty" bson:"price,omitempty"`
-
-	CreatedAt string `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	UpdatedAt string `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
--- api/internal/repository/stock-repository.go
+++ api/internal/repository/stock-repository.go
@@ -2,8 +2,11 @@ package repository
 
 import (
 	"context"
+	"log"
 	"stocktrader/internal/models"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -23,4 +26,27 @@ func (s *Stock) Create(stock models.Stock) (primitive.ObjectID, error) {
 	}
 
 	return primitive.NilObjectID, nil
+}
+
+func (s *Stock) List() ([]models.Stock, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cursor, err := s.MongoDB.Collection("stocks").Find(ctx, bson.D{})
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var stocks []models.Stock
+	err = cursor.All(ctx, &stocks)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, err
+		}
+		return nil, err
+	}
+
+	return stocks, nil
 }
