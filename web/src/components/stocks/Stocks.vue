0da9dc3212fd95a40d4f89e4d4f40445677096f5
Junior Paula <luke.junnior@icloud.com>
2023-10-24 18:06:19 -0300

    [WEB] create stock component

--- web/src/components/stocks/Stocks.vue
+++ web/src/components/stocks/Stocks.vue
@@ -1,10 +1,24 @@
 <template>
-    <h1>Ações (Stock)</h1>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id" />
+    </v-layout>
 </template>
 
 <script>
-export default {
+import Stock from './Stock.vue';
 
+export default {
+    components: { Stock },
+    data() {
+        return {
+            stocks: [
+                { id: 1, name: 'Google', price: 110 },
+                { id: 2, name: 'BMW', price: 320 },
+                { id: 3, name: 'NuBank', price: 90 },
+                { id: 1, name: 'Apple', price: 160 },
+            ]
+        }
+    }
 }
 </script>
 
